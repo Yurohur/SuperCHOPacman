4e2d837e4c19b9511ab40541b35d5c39b8d46a32
fastjech <fastjech@10.17.2.2>
2011-12-07 12:52:23 +0100

    Aus Textdateien werden Zeilen ausgelesen und in String geschrieben

--- src/superchopacman/LKonstanten.java
+++ src/superchopacman/LKonstanten.java
@@ -1,5 +1,7 @@
 package superchopacman;
 
+import java.io.*;
+
 /**
  *
  * @author fastjech
@@ -7,69 +9,43 @@ package superchopacman;
 public final class LKonstanten {
 
     public static String Labyrinth1() {
-        return  "xxxxxxxxxxxxxxx" + // 0
-                "xxxxxxxxxxxxxxx" + // 1
-                "xx..R.........x" + // 2
-                "xx.xxxx.xxxxx.x" + // 3
-                "xx.xxxx.xxxxx.x" + // 4
-                "xx.xxxx.xxxxx.x" + // 5
-                "xx............." + // 6
-                "xx.xxxx.xx.xxxx" + // 7
-                "xx.xxxx.xx.xxxx" + // 8
-                "xx............x" + // 9
-                "xxxxxxx.xxxxx.x" + // 10
-                "x.....x.xxxxx.x" + // 11
-                "x.xxx.x.xx....." + // 12
-                "x.xxx.x.xx.xxxx" + // 13
-                "x.xxx.x.xx.xxxx" + // 14
-                "xV.........xxxx" + // 15
-                "x.xxx.x.xx.xxxx" + // 16
-                "x.xxx.x.xx.xxxx" + // 17
-                "x.xxx.x.xx....." + // 18
-                "x.....x.xxxxx.x" + // 19
-                "xxxxxxx.xxxxx.x" + // 20
-                "xx............x" + // 21
-                "xx.xxxx.xx.xxxx" + // 22
-                "xx.xxxx.xx.xxxx" + // 23
-                "xx....G........" + // 24
-                "xx.xxxx.xxxxx.x" + // 25
-                "xx.xxxx.xxxxx.x" + // 26
-                "xx.xxxx.xxxxx.x" + // 27
-                "xx............x" + // 28
-                "xxxxxxxxxxxxxxx" + // 29
-                "xxxxxxxxxxxxxxx";  // 30 
+        String Labyrinth1 = "";
+
+        try {
+            String zeile;
+            StringBuffer puffer = new StringBuffer();
+            FileReader filereader = new FileReader("Labyrinth1.txt");
+            BufferedReader reader = new BufferedReader(filereader);
+            while ((zeile = reader.readLine()) != null) {
+                puffer.append(zeile);
+            }
+            reader.close();
+            Labyrinth1 = puffer.toString();
+        } catch (Exception leseFehler) {
+            // leseFehler.printStackTrace(); 
+            throw new RuntimeException("Fehler beim Einlesen der Labyrinth1 Textdatei", leseFehler);
+        }
+        return Labyrinth1;
     }
+
+    
     public static String Labyrinth2() {
-    return  "xxxxxxxxxxxxxxx" + // 0
-            "x.....x.......x" + // 1
-            "x.xxx...xxxxx.x" + // 2
-            "x.....x.......x" + // 3
-            "xxx.xxxxxxxxxxx" + // 4
-            "x.............." + // 5
-            "x.xx.x.xx.xxxxx" + // 6
-            "x.xx.x.xx.xxxxx" + // 7
-            "x....x.xx......" + // 8
-            "xx.xxx.xx.xxxxx" + // 9
-            "x.........xxxxx" + // 10
-            "x.xx.xxxx.xxxxx" + // 11
-            "x.xx.xxxx......" + // 12
-            "x.xx.xxxxx.xxxx" + // 13
-            "x.xx.xxxxx.xxxx" + // 14
-            "x..........xxxx" + // 15
-            "xx.xxx.xxx.xxxx" + // 16
-            "x....x.xxx.xxxx" + // 17
-            "x.xx.x.xx......" + // 18
-            "x.xx.x.xx.xxxxx" + // 19
-            "x....x....xxxxx" + // 20
-            "xx.xxx.xx.xxxxx" + // 21
-            "xx.xxx.xx......" + // 22
-            "xx.xxx.xxxxxx.x" + // 23
-            "x..........xx.x" + // 24
-            "x.xxxx.xxx.xx.." + // 25
-            "x.xxxx.x......x" + // 26
-            "x.xxxx.x.xxxx.x" + // 27
-            "x.............x" + // 28
-            "xxxxxxxxxxxxxxx" + // 29
-            "xxxxxxxxxxxxxxx";  // 30
+        String Labyrinth2 = "";
+
+        try {
+            String zeile;
+            StringBuffer puffer = new StringBuffer();
+            FileReader filereader = new FileReader("Labyrinth2.txt");
+            BufferedReader reader = new BufferedReader(filereader);
+            while ((zeile = reader.readLine()) != null) {
+                puffer.append(zeile);
+            }
+            reader.close();
+            Labyrinth2 = puffer.toString();
+        } catch (Exception leseFehler) {
+            // leseFehler.printStackTrace(); 
+            throw new RuntimeException("Fehler beim Einlesen der Labyrinth2 Textdatei", leseFehler);
+        }
+        return Labyrinth2;
     }
 }
